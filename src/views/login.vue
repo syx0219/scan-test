5f3915766d89cab374c357703c902b3f63c0ae4a
sunyaxin <13039998526@163.com>
2025-07-16 18:20:53 +0800

    登录页面1.8

--- src/views/login.vue
+++ src/views/login.vue
@@ -58,7 +58,7 @@ const validatePass = (value) => {
 }
 
 const onSubmit = async (values) => {
-  alert(values)
+  alert(JSON.stringify(values))
   if (values.password === '') {
     return showToast('password cannot be empty')
   }
