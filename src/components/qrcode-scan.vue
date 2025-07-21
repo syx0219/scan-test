baab7ccc4d866e00959b0614833718f64d01745c
sunyaxin <13039998526@163.com>
2025-07-21 12:10:05 +0800

    初始化摄像聚焦2.3

--- src/components/qrcode-scan.vue
+++ src/components/qrcode-scan.vue
@@ -91,23 +91,24 @@ const initCamera = async () => {
 
     // 未获取权限时的处理
     if (!hasPermission) {
-      let stream = await navigator.mediaDevices.getUserMedia({
-        video: {
-          facingMode: cameraType.value,
-          autoFocus: true,
-          width: 1200,
-          height: 800,
-        },
-      })
+      alert(JSON.stringify(videoDevices))
+      // let stream = await navigator.mediaDevices.getUserMedia({
+      //   video: {
+      //     facingMode: cameraType.value,
+      //     autoFocus: true,
+      //     width: 1200,
+      //     height: 800,
+      //   },
+      // })
 
       // 停止初始化的媒体流（仅用于触发权限）
-      stream.getTracks().forEach((track) => track.stop())
+      // stream.getTracks().forEach((track) => track.stop())
 
       // 重新枚举获取完整设备列表
-      devices = await navigator.mediaDevices.enumerateDevices()
-      videoDevices = devices.filter((device) => device.kind === 'videoinput')
-      deviceArr = videoDevices.filter((device) => device.label === 'camera2 3, facing back')
-      alert(JSON.stringify(deviceArr))
+      // devices = await navigator.mediaDevices.enumerateDevices()
+      // videoDevices = devices.filter((device) => device.kind === 'videoinput')
+      // deviceArr = videoDevices.filter((device) => device.label === 'camera2 3, facing back')
+
       // updateCameraConfig(deviceArr)
 
       // 请求权限并获取媒体流
